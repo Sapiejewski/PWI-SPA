489f8fad8e7b287a9f865dd99de87569f8753bcf
Sapiejewski <fsapiejewski01@gmail.com>
2024-06-08 19:40:43 +0200

    cookie accept button text fixed

--- src/components/Cookies/Cookies.js
+++ src/components/Cookies/Cookies.js
@@ -24,7 +24,7 @@ const Cookies = () => {
 				<button
 					onClick={handleCookieConsent}
 					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-					{t("CookieAccept")}
+					{t("CookiesAccept")}
 				</button>
 			</div>
 		)
